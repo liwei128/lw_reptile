cbd003f473da164d7d97f35476cae7e2c3e9668e
wei.li <wei.li@flightroutes24.com>
2017-11-25 14:19:34 +0800

    优化性能

--- src/main/java/com/abner/pojo/Record.java
+++ src/main/java/com/abner/pojo/Record.java
@@ -1,12 +1,8 @@
 package com.abner.pojo;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import com.alibaba.fastjson.JSON;
 /**
  * 单个监控实体类
  * @author wei.li
@@ -14,25 +10,20 @@ import com.alibaba.fastjson.JSON;
  */
 public class Record {
 	
-	private List<String> times = new ArrayList<>();
+	private List<Long> times = new ArrayList<>();
 	
 	public long getNumByMinutes(int minutes){
 		if(minutes==0){
 			return times.size();
 		}
-		try {
-			return getNumByMillis(minutes*60*1000);
-		} catch (ParseException e) {
-			return 0L;
-		}
+		return getNumByMillis(minutes*60*1000);
 	}
-	private long getNumByMillis(long timeMillis) throws ParseException{
+	
+	private long getNumByMillis(long timeMillis){
 		long num=0L;
 		long nowTime = System.currentTimeMillis();
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 		for(int i=0;i<times.size();i++){
-			long createTime = simpleDateFormat.parse(times.get(i)).getTime();
-			if(nowTime-createTime<timeMillis){
+			if(nowTime-times.get(i)<timeMillis){
 				num++;
 			}
 		}
@@ -40,17 +31,10 @@ public class Record {
 	}
 	
 	public void add(int num){
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		long currentTimeMillis = System.currentTimeMillis();
 		for(int i=0;i<num;i++){
-			Date date = new Date();
-			String formatDate = simpleDateFormat.format(date);
-			times.add(formatDate);
+			times.add(currentTimeMillis);
 		}
-	}
-
-	@Override
-	public String toString() {
-		return JSON.toJSONString(times);
 	}
 	
 	
