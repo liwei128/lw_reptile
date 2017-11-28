88254cc24ddeba2b28857c6343e8e67a91366445
wei.li <wei.li@flightroutes24.com>
2017-11-28 17:18:01 +0800

    wwww

--- src/main/java/com/abner/Client.java
+++ src/main/java/com/abner/Client.java
@@ -9,7 +9,7 @@ import com.abner.view.ReptileView;
  */
 public class Client {
 	
-	public static void main(String[] args) {gitgit
+	public static void main(String[] args) {
 		//开启视图
 		String df="12324324";
 
