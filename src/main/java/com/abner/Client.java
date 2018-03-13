3f79605931bce1da253e588ac2a5ac36254079f4
Your Name <you@example.com>
2018-03-13 16:14:10 +0800

    ss

--- src/main/java/com/abner/Client.java
+++ src/main/java/com/abner/Client.java
@@ -2,15 +2,11 @@ package com.abner;
 
 import com.abner.view.ReptileView;
 
-/**
- * 程序入口
- * @author wei.li
- * @time 2017年6月19日下午3:05:03
- */
+
 public class Client {
-	
+
 	public static void main(String[] args) {
 		//开启视图
-		new ReptileView();
+		/*new ReptileView();*/
 	}
 }
